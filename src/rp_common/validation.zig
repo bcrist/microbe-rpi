be4d2b5d6c0fa0234f42cbc435d9eeabb46619b8
Ben Crist <github@magicmoremagic.com>
2024-08-17 01:30:31 -0500

    Remove Comptime_Resource_Validator; no longer allowed by zig

--- src/rp_common/validation.zig
+++ src/rp_common/validation.zig
@@ -1,5 +1,5 @@
-pub const pads = microbe.Comptime_Resource_Validator(chip.Pad_ID, "pad");
-pub const dma = microbe.Comptime_Resource_Validator(chip.dma.Channel, "DMA channel");
+pub const pads = microbe.Bitset_Resource_Validator(chip.Pad_ID, "pad");
+pub const dma = microbe.Bitset_Resource_Validator(chip.dma.Channel, "DMA channel");
 
 const chip = @import("chip");
 const microbe = @import("microbe");
